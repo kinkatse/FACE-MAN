267a89a6ea6e427fc2a71cfc4b29dcc15079bba4
kakintse <kinkatse@mail.adelphi.edu>
2021-07-29 14:27:23 -0400

    Style the buttons

--- src/script.js
+++ src/script.js
@@ -85,14 +85,16 @@ function draw() {
   if (face !== undefined) {
     image(video, 0, 0, width, height);
 
-  if (face.boundingBox.bottomRight[0] - face.boundingBox.topLeft[0] - 40 < 125 || face.boundingBox.bottomRight[1] - face.boundingBox.topLeft[1] + 110 < 200) {
-      console.log("Error: Bring your face closer and keep it straight");
-      // return (<span class="Error">Error: Bring your face closer and keep it straight</span>);
-  }
-  if (face.boundingBox.bottomRight[0] - face.boundingBox.topLeft[0] - 40 > 300 || face.boundingBox.bottomRight[1] - face.boundingBox.topLeft[1] + 110 > 375) {
-      console.log("Error: Back up a bit and keep your head straight");
-      // return (<span class="Error">Error: Back up a bit and keep your head straight</span>);
-  }
+    // function error() {
+      if (face.boundingBox.bottomRight[0] - face.boundingBox.topLeft[0] - 40 < 125 || face.boundingBox.bottomRight[1] - face.boundingBox.topLeft[1] + 110 < 200) {
+          return console.log("Error: Bring your face closer and keep it straight");
+          // return (<span class="Error">Error: Bring your face closer and keep it straight</span>);
+      }
+      if (face.boundingBox.bottomRight[0] - face.boundingBox.topLeft[0] - 40 > 300 || face.boundingBox.bottomRight[1] - face.boundingBox.topLeft[1] + 110 > 375) {
+          return console.log("Error: Back up a bit and keep your head straight");
+          // return (<span class="Error">Error: Back up a bit and keep your head straight</span>);
+      }
+    // }
 
     if (firstFace) {
       console.log(face);
