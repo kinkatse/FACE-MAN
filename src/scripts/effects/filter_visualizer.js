3bcbf1ac1b1d0059b6537b8b6944e94e6426fa2b
kakintse <kinkatse@mail.adelphi.edu>
2021-09-07 23:55:01 -0400

    Added colors to different main filters

--- src/scripts/effects/filter_visualizer.js
+++ src/scripts/effects/filter_visualizer.js
@@ -1,38 +1,42 @@
 let fv_x;
-let fv_y;
 let fv_dist;
 let fv_word;
+let fv_color = 'rgba(132, 255, 181, 0.3)';
 
 function filter_vis(filter) {
 
-    if (filter === "hitbox") {
-        fv_x = 35;
-        fv_dist = 70;
-        fv_word = "Hitbox";
-    }
-
     if (filter === "scanmask") {
-        fv_x = 126;
+        fv_x = 50;
         fv_dist = 100;
         fv_word = "Scanmask";
+        fv_color = 'rgba(191, 225, 255, 0.3)';
     }
 
     if (filter === "pacman") {
-        fv_x = 126;
+        fv_x = 50;
         fv_dist = 100;
         fv_word = "Pacman";
+        fv_color = 'rgba(255, 245, 140, 0.3)';
     }
 
     if (filter === "kirby") {
-        fv_x = 126;
+        fv_x = 50;
         fv_dist = 100;
         fv_word = "Kirby";
+        fv_color = 'rgba(255, 186, 212, 0.3)';
     }
 
     if (filter === "pikachu") {
-        fv_x = 126;
+        fv_x = 50;
         fv_dist = 100;
         fv_word = "Pikachu";
+        fv_color = 'rgba(255, 228, 165, 0.3)';
+    }
+
+    if (filter === "hitbox") {
+        fv_x = 50;
+        fv_dist = 70;
+        fv_word = "Hitbox";
     }
 
     if (filter === "pretty") {
@@ -56,7 +60,7 @@ function filter_vis(filter) {
     // Filter is on visualizer
     push();
     translate(fv_x, 15);
-    fill('rgba(191, 225, 255, 0.3)');
+    fill(fv_color);
     stroke(153, 207, 255);
     strokeWeight(2);
     rectMode(CENTER);
