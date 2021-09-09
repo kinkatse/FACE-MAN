0a60ca1e4316c61a4af5813359b4294b5f296497
kakintse <kinkatse@mail.adelphi.edu>
2021-09-09 12:09:39 -0400

    Attempted to make icons into buttons to toggle off filters

--- src/scripts/effects/filter_positioning.js
+++ src/scripts/effects/filter_positioning.js
@@ -1,4 +1,6 @@
-function FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg) {
+// let button;
+
+function FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle) {
     
     push();
     translate(23, fvY);
@@ -7,6 +9,9 @@ function FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg) {
     strokeWeight(2);
     rectMode(CENTER);
     rect(1, 1, fvDist, 30, 5, 5, 5, 5);
+    // button = createButton('hi');
+    // button.position(300, 300);
+    // button.mousePressed(toggle);
     fill(255);
     imageMode(CENTER);
     image(fvImg, 0, 0, 30, 30);
--- src/scripts/effects/filter_visualizer.js
+++ src/scripts/effects/filter_visualizer.js
@@ -4,6 +4,7 @@ let fvImg;
 let fvColor1;
 let fvColor2;
 let filterPos = [17, 52, 87, 122, 157]
+let toggle;
 
 function FilterVis(positions) {
     for (let i = 0; i < positions.length; i++) {
@@ -12,49 +13,57 @@ function FilterVis(positions) {
             fvColor1 = 'rgba(191, 225, 255, 1)';
             fvColor2 = 'rgb(153, 207, 255)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = faceMaskDotsButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "pacman") {
             fvImg = pacmanIcon;
             fvColor1 = 'rgba(255, 245, 140, 1)';
             fvColor2 = 'rgb(229, 214, 75)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = pacmanButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "kirby") {
             fvImg = kirbyIcon;
             fvColor1 = 'rgba(255, 186, 212, 1)';
             fvColor2 = 'rgb(234, 140, 176)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = kirbyButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "pikachu") {
             fvImg = pikachuIcon;
             fvColor1 = 'rgba(255, 228, 165, 1)';
             fvColor2 = 'rgb(219, 179, 87)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = pikachuButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "hitbox") {
             fvImg = hitboxIcon;
             fvColor1 = 'rgba(132, 255, 181, 1)';
             fvColor2 = 'rgb(74, 206, 125)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = hitboxButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "pretty") {
             fvImg = prettyIcon;
             fvColor1 = 'rgba(132, 255, 181, 1)';
             fvColor2 = 'rgb(74, 206, 125)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = prettyButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "mustache") {
             fvImg = mustacheIcon;
             fvColor1 = 'rgba(132, 255, 181, 1)';
             fvColor2 = 'rgb(74, 206, 125)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = mustacheButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         } else if (positions[i] === "glasses") {
             fvImg = glassesIcon;
             fvColor1 = 'rgba(132, 255, 181, 1)';
             fvColor2 = 'rgb(74, 206, 125)';
             fvY = filterPos[i];
-            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg);
+            toggle = glassesButton;
+            new FilterPos(fvY, fvColor1, fvColor2, fvDist, fvImg, toggle);
         }
     }
 }
